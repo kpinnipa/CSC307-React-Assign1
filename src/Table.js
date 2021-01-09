5cd3c846e12d6334785db67dbfc8d8847fccce8d
Kiran Pinnipati <kpinnipati@gmail.com>
2021-01-08 20:54:23 -0800

    Fixed table and added delete feature.

--- src/Table.js
+++ src/Table.js
@@ -17,6 +17,9 @@ const TableBody = props => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
@@ -25,16 +28,15 @@ const TableBody = props => {
     )
 }
 
-class Table extends Component {
-  render() {
-      const { characterData } = this.props
+const Table = props => {
+    const { characterData, removeCharacter } = props
+  
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={characterData} />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
       </table>
     )
-  }
 }
 
 export default Table
