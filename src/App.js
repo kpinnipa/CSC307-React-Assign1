bd1794d457e1b6f1826d2c7124bfb6147e960425
Kiran Pinnipati <kpinnipati@gmail.com>
2021-01-15 11:10:14 -0800

    Link established.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import Table from './Table'
-import Form from './Form'
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
+import axios from 'axios';
 
 class App extends Component {
     state = {
@@ -20,6 +21,18 @@ class App extends Component {
     handleSubmit = character => {
         this.setState({characters: [...this.state.characters,character]})
     }
+
+  componentDidMount() {
+      axios.get('http://localhost:5000/users')
+       .then(res => {
+         const characters = res.data.users_list;
+         this.setState({ characters });
+       })
+       .catch(function (error) {
+         //Not handling the error. Just logging into the console.
+         console.log(error);
+       });
+  }
 
   render() {
     const { characters } = this.state;
