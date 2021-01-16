fd90b29e53795187ec6fb99b030f6b23b8f742e9
Kiran Pinnipati <kpinnipati@gmail.com>
2021-01-16 00:36:34 -0800

    Finished post method attached to submit button

--- src/App.js
+++ src/App.js
@@ -17,9 +17,25 @@ class App extends Component {
           }),
         })
     }
+    
+    makePostCall(character){
+      return axios.post('http://localhost:5000/users', character)
+       .then(function (response) {
+         console.log(response);
+         return (response.status === 200);
+       })
+       .catch(function (error) {
+         console.log(error);
+         return false;
+       });
+    }
 
     handleSubmit = character => {
-        this.setState({characters: [...this.state.characters,character]})
+      this.makePostCall(character).then( callResult => {
+         if (callResult === true) {
+            this.setState({ characters: [...this.state.characters, character] });
+         }
+      });
     }
 
   componentDidMount() {
